37dea9a18592ff1629f80d546b9b74e351655817
sonbui <sonbui.gfd@gmail.com>
2019-09-01 07:34:32 +0700

    update section faq and pricing

--- src/js/main.js
+++ src/js/main.js
@@ -18,11 +18,46 @@
       interval: 100
     })
 
-    sr.reveal('.media-canvas', {
+    sr.reveal('.media-canvas, .pricing-faqs,', {
       duration: 600,
       scale: '.95',
       easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
       viewFactor: 0.5
+    })
+
+    const pricingTables = document.querySelectorAll('.pricing-table')
+
+    pricingTables.forEach(pricingTable => {
+      const pricingTableHeader = [].slice.call(
+        pricingTable.querySelectorAll('.pricing-table-header')
+      )
+      const pricingTableList = [].slice.call(
+        pricingTable.querySelectorAll('.pricing-table-features li')
+      )
+      const pricingTableCta = [].slice.call(
+        pricingTable.querySelectorAll('.pricing-table-cta')
+      )
+      const elements = pricingTableHeader
+        .concat(pricingTableList)
+        .concat(pricingTableCta)
+
+      sr.reveal(elements, {
+        duration: 600,
+        distance: '20px',
+        easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
+        interval: 100,
+        origin: 'bottom',
+        viewFactor: 0.5
+      })
+    })
+
+    sr.reveal('.tabs-links li, .pricing-faqs, .cta-inner', {
+      duration: 600,
+      distance: '40px',
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      interval: 100,
+      origin: 'bottom',
+      viewFactor: 0.2
     })
   }
 
@@ -37,6 +72,51 @@
     deviceMockupLoaded()
   } else {
     deviceMockup.addEventListener('load', deviceMockupLoaded)
+  }
+
+  // Accordion component
+  const accordionEl = document.getElementsByClassName('accordion-title')
+
+  if (accordionEl.length) {
+    for (let i = 0; i < accordionEl.length; i++) {
+      accordionEl[i].addEventListener('click', function () {
+        this.parentNode.classList.toggle('is-open')
+        const panel = this.nextElementSibling
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null
+        } else {
+          panel.style.maxHeight = `${panel.scrollHeight}px`
+        }
+      })
+    }
+  }
+
+  // Tabs component
+  const tabLinksAll = document.getElementsByClassName('tab-link')
+
+  if (tabLinksAll.length) {
+    for (let i = 0; i < tabLinksAll.length; i++) {
+      tabLinksAll[i].addEventListener('click', function (e) {
+        e.preventDefault()
+        let tabLinksContainer = tabLinksAll[i].parentNode.parentNode
+        let tabPanels = tabLinksContainer.nextElementSibling.getElementsByClassName(
+          'tab-panel'
+        )
+        let tabLinks = tabLinksContainer.getElementsByClassName('tab-link')
+        // Remove is-active class from all links and panels
+        for (let i = 0; i < tabLinks.length; i++) {
+          tabLinks[i].classList.remove('is-active')
+        }
+        for (let i = 0; i < tabPanels.length; i++) {
+          tabPanels[i].classList.remove('is-active')
+        }
+        // Get the ID of panel to display
+        let tabID = this.getAttribute('href')
+        // Add is-active class to matching link and panel
+        tabLinksAll[i].classList.add('is-active')
+        document.querySelector(tabID).classList.add('is-active')
+      })
+    }
   }
 
   // Features title adjustment
