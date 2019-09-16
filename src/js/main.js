86a652e30a7bf28aec9db3c7bcbfeaa5fc2bdd51
sonbui <sonbui.gfd@gmail.com>
2019-09-16 08:45:27 +0700

    update sections

--- src/js/main.js
+++ src/js/main.js
@@ -9,28 +9,78 @@
     /* global ScrollReveal */
     const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.hero-title, .hero-paragraph, .hero-cta', {
-      delay: 150,
-      duration: 1000,
-      distance: '60px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'bottom',
-      interval: 150
+    /* global anime */
+    const heroAnimation = anime.timeline({ autoplay: false })
+    const strokedElement = document.querySelector('.stroke-animation')
+
+    strokedElement.setAttribute('stroke-dashoffset', anime.setDashoffset(strokedElement))
+
+    heroAnimation.add({
+      targets: '.stroke-animation',
+      strokeDashoffset: {
+        value: 0,
+        duration: 2000,
+        easing: 'easeInOutQuart'
+      },
+      strokeWidth: {
+        value: [0, 2],
+        duration: 2000,
+        easing: 'easeOutCubic'
+      },
+      strokeOpacity: {
+        value: [1, 0],
+        duration: 1000,
+        easing: 'easeOutCubic',
+        delay: 1000
+      },
+      fillOpacity: {
+        value: [0, 1],
+        duration: 500,
+        easing: 'easeOutCubic',
+        delay: 1300
+      }
+    }).add({
+      targets: '.fadeup-animation',
+      offset: 1300, // Starts at 1300ms of the timeline
+      translateY: {
+        value: [100, 0],
+        duration: 1500,
+        easing: 'easeOutElastic',
+        delay: function (el, i) {
+          return i * 150
+        }
+      },
+      opacity: {
+        value: [0, 1],
+        duration: 200,
+        easing: 'linear',
+        delay: function (el, i) {
+          return i * 150
+        }
+      }
+    }).add({
+      targets: '.fadeleft-animation',
+      offset: 1300, // Starts at 1300ms of the timeline
+      translateX: {
+        value: [40, 0],
+        duration: 400,
+        easing: 'easeOutCubic',
+        delay: function (el, i) {
+          return i * 100
+        }
+      },
+      opacity: {
+        value: [0, 1],
+        duration: 200,
+        easing: 'linear',
+        delay: function (el, i) {
+          return i * 100
+        }
+      }
     })
 
-    sr.reveal('.hero-right-decoration', {
-      duration: 1000,
-      distance: '40px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'top'
-    })
-
-    sr.reveal('.hero-left-decoration', {
-      duration: 1000,
-      distance: '40px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      origin: 'bottom'
-    })
+    doc.classList.add('anime-ready')
+    heroAnimation.play()
 
     sr.reveal('.clients li', {
       delay: 300,
