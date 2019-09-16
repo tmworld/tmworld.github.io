27b18967f54899e7365f93a5f102b42733475034
sonbui <sonbui.gfd@gmail.com>
2019-09-16 09:13:06 +0700

    update navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
-(function () {
+;(function () {
+  const $ = window.jQuery
   const doc = document.documentElement
 
   doc.classList.remove('no-js')
@@ -7,77 +8,83 @@
   // Reveal animations
   if (document.body.classList.contains('has-animations')) {
     /* global ScrollReveal */
-    const sr = window.sr = ScrollReveal()
+    const sr = (window.sr = ScrollReveal())
 
     /* global anime */
     const heroAnimation = anime.timeline({ autoplay: false })
     const strokedElement = document.querySelector('.stroke-animation')
 
-    strokedElement.setAttribute('stroke-dashoffset', anime.setDashoffset(strokedElement))
+    strokedElement.setAttribute(
+      'stroke-dashoffset',
+      anime.setDashoffset(strokedElement)
+    )
 
-    heroAnimation.add({
-      targets: '.stroke-animation',
-      strokeDashoffset: {
-        value: 0,
-        duration: 2000,
-        easing: 'easeInOutQuart'
-      },
-      strokeWidth: {
-        value: [0, 2],
-        duration: 2000,
-        easing: 'easeOutCubic'
-      },
-      strokeOpacity: {
-        value: [1, 0],
-        duration: 1000,
-        easing: 'easeOutCubic',
-        delay: 1000
-      },
-      fillOpacity: {
-        value: [0, 1],
-        duration: 500,
-        easing: 'easeOutCubic',
-        delay: 1300
-      }
-    }).add({
-      targets: '.fadeup-animation',
-      offset: 1300, // Starts at 1300ms of the timeline
-      translateY: {
-        value: [100, 0],
-        duration: 1500,
-        easing: 'easeOutElastic',
-        delay: function (el, i) {
-          return i * 150
+    heroAnimation
+      .add({
+        targets: '.stroke-animation',
+        strokeDashoffset: {
+          value: 0,
+          duration: 2000,
+          easing: 'easeInOutQuart'
+        },
+        strokeWidth: {
+          value: [0, 2],
+          duration: 2000,
+          easing: 'easeOutCubic'
+        },
+        strokeOpacity: {
+          value: [1, 0],
+          duration: 1000,
+          easing: 'easeOutCubic',
+          delay: 1000
+        },
+        fillOpacity: {
+          value: [0, 1],
+          duration: 500,
+          easing: 'easeOutCubic',
+          delay: 1300
         }
-      },
-      opacity: {
-        value: [0, 1],
-        duration: 200,
-        easing: 'linear',
-        delay: function (el, i) {
-          return i * 150
+      })
+      .add({
+        targets: '.fadeup-animation',
+        offset: 1300, // Starts at 1300ms of the timeline
+        translateY: {
+          value: [100, 0],
+          duration: 1500,
+          easing: 'easeOutElastic',
+          delay: function (el, i) {
+            return i * 150
+          }
+        },
+        opacity: {
+          value: [0, 1],
+          duration: 200,
+          easing: 'linear',
+          delay: function (el, i) {
+            return i * 150
+          }
         }
-      }
-    }).add({
-      targets: '.fadeleft-animation',
-      offset: 1300, // Starts at 1300ms of the timeline
-      translateX: {
-        value: [40, 0],
-        duration: 400,
-        easing: 'easeOutCubic',
-        delay: function (el, i) {
-          return i * 100
+      })
+      .add({
+        targets: '.fadeleft-animation',
+        offset: 1300, // Starts at 1300ms of the timeline
+        translateX: {
+          value: [40, 0],
+          duration: 400,
+          easing: 'easeOutCubic',
+          delay: function (el, i) {
+            return i * 100
+          }
+        },
+        opacity: {
+          value: [0, 1],
+          duration: 200,
+          easing: 'linear',
+          delay: function (el, i) {
+            return i * 100
+          }
         }
-      },
-      opacity: {
-        value: [0, 1],
-        duration: 200,
-        easing: 'linear',
-        delay: function (el, i) {
-          return i * 100
-        }
-      }
-    })
+      })
 
     doc.classList.add('anime-ready')
     heroAnimation.play()
@@ -92,14 +99,17 @@
       interval: 150
     })
 
-    sr.reveal('.feature, .tabs-links li, .testimonial, .pricing-table, .pricing-faqs, .cta-inner', {
-      duration: 600,
-      distance: '40px',
-      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-      interval: 100,
-      origin: 'bottom',
-      viewFactor: 0.2
-    })
+    sr.reveal(
+      '.feature, .tabs-links li, .testimonial, .pricing-table, .pricing-faqs, .cta-inner',
+      {
+        duration: 600,
+        distance: '40px',
+        easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        interval: 100,
+        origin: 'bottom',
+        viewFactor: 0.2
+      }
+    )
   }
 
   // Accordion component
@@ -127,7 +137,9 @@
       tabLinksAll[i].addEventListener('click', function (e) {
         e.preventDefault()
         let tabLinksContainer = tabLinksAll[i].parentNode.parentNode
-        let tabPanels = tabLinksContainer.nextElementSibling.getElementsByClassName('tab-panel')
+        let tabPanels = tabLinksContainer.nextElementSibling.getElementsByClassName(
+          'tab-panel'
+        )
         let tabLinks = tabLinksContainer.getElementsByClassName('tab-link')
         // Remove is-active class from all links and panels
         for (let i = 0; i < tabLinks.length; i++) {
@@ -144,4 +156,35 @@
       })
     }
   }
-}())
+
+  $('.smootscroll').on('click', function (e) {
+    e.preventDefault()
+    var sec = $(this).attr('href')
+    $('html, body').animate(
+      {
+        scrollTop: $(sec).offset().top
+      },
+      500
+    )
+
+    $('.nav-icon.site-header__control').removeClass('open')
+    $('.site-header').removeClass('open')
+    if ($(window).width() <= 768) {
+      $('.site-header__nav')
+        .stop()
+        .slideUp()
+    }
+  })
+
+  $(document).ready(function () {
+    $('.nav-icon.site-header__control').click(function () {
+      $(this).toggleClass('open')
+      console.log('12312')
+      $('.site-header').toggleClass('open')
+
+      $('.site-header__nav')
+        .stop()
+        .slideToggle()
+    })
+  })
+})()
