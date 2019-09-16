5e789166e81f773feb710d38b2ff4d0298ab10d7
sonbui <sonbui.gfd@gmail.com>
2019-09-16 07:53:02 +0700

    init new layout

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,4 @@
-;(function () {
-  const $ = window.jQuery
-  const win = window
+(function () {
   const doc = document.documentElement
 
   doc.classList.remove('no-js')
@@ -9,50 +7,42 @@
   // Reveal animations
   if (document.body.classList.contains('has-animations')) {
     /* global ScrollReveal */
-    const sr = (window.sr = ScrollReveal())
+    const sr = window.sr = ScrollReveal()
 
-    sr.reveal('.feature', {
-      duration: 600,
-      distance: '20px',
-      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      origin: 'right',
-      interval: 100
+    sr.reveal('.hero-title, .hero-paragraph, .hero-cta', {
+      delay: 150,
+      duration: 1000,
+      distance: '60px',
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      origin: 'bottom',
+      interval: 150
     })
 
-    sr.reveal('.media-canvas, .pricing-faqs,', {
-      duration: 600,
-      scale: '.95',
-      easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-      viewFactor: 0.5
+    sr.reveal('.hero-right-decoration', {
+      duration: 1000,
+      distance: '40px',
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      origin: 'top'
     })
 
-    const pricingTables = document.querySelectorAll('.pricing-table')
-
-    pricingTables.forEach(pricingTable => {
-      const pricingTableHeader = [].slice.call(
-        pricingTable.querySelectorAll('.pricing-table-header')
-      )
-      const pricingTableList = [].slice.call(
-        pricingTable.querySelectorAll('.pricing-table-features li')
-      )
-      const pricingTableCta = [].slice.call(
-        pricingTable.querySelectorAll('.pricing-table-cta')
-      )
-      const elements = pricingTableHeader
-        .concat(pricingTableList)
-        .concat(pricingTableCta)
-
-      sr.reveal(elements, {
-        duration: 600,
-        distance: '20px',
-        easing: 'cubic-bezier(0.5, -0.01, 0, 1.005)',
-        interval: 100,
-        origin: 'bottom',
-        viewFactor: 0.5
-      })
+    sr.reveal('.hero-left-decoration', {
+      duration: 1000,
+      distance: '40px',
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      origin: 'bottom'
     })
 
-    sr.reveal('.tabs-links li, .pricing-faqs, .cta-inner', {
+    sr.reveal('.clients li', {
+      delay: 300,
+      duration: 1000,
+      rotate: {
+        y: 50
+      },
+      easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+      interval: 150
+    })
+
+    sr.reveal('.feature, .tabs-links li, .testimonial, .pricing-table, .pricing-faqs, .cta-inner', {
       duration: 600,
       distance: '40px',
       easing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
@@ -60,19 +50,6 @@
       origin: 'bottom',
       viewFactor: 0.2
     })
-  }
-
-  // Wait that device mockup has loaded before displaying
-  const deviceMockup = document.querySelector('.device-mockup')
-
-  function deviceMockupLoaded () {
-    deviceMockup.classList.add('has-loaded')
-  }
-
-  if (deviceMockup.complete) {
-    deviceMockupLoaded()
-  } else {
-    deviceMockup.addEventListener('load', deviceMockupLoaded)
   }
 
   // Accordion component
@@ -100,9 +77,7 @@
       tabLinksAll[i].addEventListener('click', function (e) {
         e.preventDefault()
         let tabLinksContainer = tabLinksAll[i].parentNode.parentNode
-        let tabPanels = tabLinksContainer.nextElementSibling.getElementsByClassName(
-          'tab-panel'
-        )
+        let tabPanels = tabLinksContainer.nextElementSibling.getElementsByClassName('tab-panel')
         let tabLinks = tabLinksContainer.getElementsByClassName('tab-link')
         // Remove is-active class from all links and panels
         for (let i = 0; i < tabLinks.length; i++) {
@@ -119,143 +94,4 @@
       })
     }
   }
-
-  // Features title adjustment
-  // const featuresSection = document.querySelector('.features')
-  // const featuresTitle = featuresSection.querySelector('.section-title')
-  // const firstFeature = document.querySelector('.feature-inner')
-
-  // featuresTitlePos()
-  // win.addEventListener('resize', featuresTitlePos)
-
-  // function featuresTitlePos () {
-  //   let featuresSectionLeft = featuresSection.querySelector('.features-inner').getBoundingClientRect().left
-  //   let firstFeatureLeft = firstFeature.getBoundingClientRect().left
-  //   let featuresTitleOffset = parseInt(firstFeatureLeft - featuresSectionLeft)
-  //   if (firstFeatureLeft > featuresSectionLeft) {
-  //     featuresTitle.style.marginLeft = `${featuresTitleOffset}px`
-  //   } else {
-  //     featuresTitle.style.marginLeft = 0
-  //   }
-  // }
-
-  // Moving objects
-  const movingObjects = document.querySelectorAll('.is-moving-object')
-
-  // Throttling
-  function throttle (func, milliseconds) {
-    let lastEventTimestamp = null
-    let limit = milliseconds
-
-    return (...args) => {
-      let now = Date.now()
-
-      if (!lastEventTimestamp || now - lastEventTimestamp >= limit) {
-        lastEventTimestamp = now
-        func.apply(this, args)
-      }
-    }
-  }
-
-  // Init vars
-  let mouseX = 0
-  let mouseY = 0
-  let scrollY = 0
-  let coordinateX = 0
-  let coordinateY = 0
-  let winW = doc.clientWidth
-  let winH = doc.clientHeight
-
-  // Move Objects
-  function moveObjects (e, object) {
-    mouseX = e.pageX
-    mouseY = e.pageY
-    scrollY = win.scrollY
-    coordinateX = winW / 2 - mouseX
-    coordinateY = winH / 2 - (mouseY - scrollY)
-
-    for (let i = 0; i < object.length; i++) {
-      const translatingFactor =
-        object[i].getAttribute('data-translating-factor') || 20
-      const rotatingFactor =
-        object[i].getAttribute('data-rotating-factor') || 20
-      const perspective = object[i].getAttribute('data-perspective') || 500
-      let tranformProperty = []
-
-      if (object[i].classList.contains('is-translating')) {
-        tranformProperty.push(
-          'translate(' +
-            coordinateX / translatingFactor +
-            'px, ' +
-            coordinateY / translatingFactor +
-            'px)'
-        )
-      }
-
-      if (object[i].classList.contains('is-rotating')) {
-        tranformProperty.push(
-          'perspective(' +
-            perspective +
-            'px) rotateY(' +
-            -coordinateX / rotatingFactor +
-            'deg) rotateX(' +
-            coordinateY / rotatingFactor +
-            'deg)'
-        )
-      }
-
-      if (
-        object[i].classList.contains('is-translating') ||
-        object[i].classList.contains('is-rotating')
-      ) {
-        tranformProperty = tranformProperty.join(' ')
-
-        object[i].style.transform = tranformProperty
-        object[i].style.transition = 'transform 1s ease-out'
-        object[i].style.transformStyle = 'preserve-3d'
-        object[i].style.backfaceVisibility = 'hidden'
-      }
-    }
-  }
-
-  // Call function with throttling
-  if (movingObjects) {
-    win.addEventListener(
-      'mousemove',
-      throttle(function (e) {
-        moveObjects(e, movingObjects)
-      }, 150)
-    )
-  }
-
-  $('.smootscroll').on('click', function (e) {
-    e.preventDefault()
-    var sec = $(this).attr('href')
-    $('html, body').animate(
-      {
-        scrollTop: $(sec).offset().top
-      },
-      500
-    )
-
-    $('.nav-icon.site-header__control').removeClass('open')
-    $('.site-header').removeClass('open')
-    if ($(window).width() <= 768) {
-      $('.site-header__nav')
-        .stop()
-        .slideUp()
-    }
-  })
-
-  $(document).ready(function () {
-    $('.nav-icon.site-header__control').click(function () {
-      $(this).toggleClass('open')
-
-      $('.site-header').toggleClass('open')
-
-      $('.site-header__nav')
-        .stop()
-        .slideToggle()
-    })
-  })
-})()
+}())
